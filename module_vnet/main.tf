9657facaf62de777fdddce63636e1706189a62e1
ProcessX <luc2526@gmail.com>
2024-03-15 11:47:07 +0100

    vnet & subnets definded

--- module_vnet/main.tf
+++ module_vnet/main.tf
@@ -4,3 +4,18 @@ resource "azurerm_virtual_network" "vnet" {
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "subnet" {
+  count = length(var.subnet_names)
+  name = var.subnet_names[count.index]
+  resource_group_name = data.azurerm_resource_group.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = [var.subnet_prefixes[count.index]]
+}
+
+locals {
+  azurerm_subnet = {
+    for index, subnet in azurerm_subnet.subnet :
+    subnet.name => subnet.id
+  }
+}
