1ac721e8cd783b7182d32ca025e34b1c3ddf63ba
jihad-bf <jihad.spiridon@gmail.com>
2024-03-29 10:03:19 +0100

    added files main, outputs and variables

--- main.tf
+++ main.tf
@@ -1,14 +1,188 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "my-ehealth-resources"
-  location = "Central US"
+resource "azurerm_resource_group" "ehealth_rg" {
+  name     = "ehealth-resources"
+  location = "West Europe"
 }
 
-module "vnet" {
-  source              = "github.com/1307bilal/Ehealth_azure"
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
-  subnet_names        = ["subnet1", "subnet2"]
 
-  depends_on = [azurerm_resource_group.rg]
+/** resource "azurerm__network_security_group" "ehealth_security_group_in_nlb_web" {
+  name                = var.network_security_group_in_nlb_web
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "http_node1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = module.module_vnet.public_ip_id
+    destination_address_prefix = "10.0.1.0/24"
+  }
+
+  security_rule {
+    name                       = "https_node1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = module.module_vnet.public_ip_id
+    destination_address_prefix = "10.0.1.0/24"
+  }
+
+  security_rule {
+    name                       = "http_node2"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = module.module_vnet.public_ip_id
+    destination_address_prefix = "10.0.4.0/24"
+  }
+
+  security_rule {
+    name                       = "https_node2"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = module.module_vnet.public_ip_id
+    destination_address_prefix = "10.0.4.0/24"
+  }
+
+}
+***/
+resource "azurerm__network_security_group" "ehealth_security_group_web" {
+  name                = var.network_security_group_web
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = module.module_vnet.public_ip_id
+    destination_address_prefix = "10.0.1.0/24"
+  }
+
+  security_rule {
+    name                       = "https"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = module.module_vnet.public_ip_id
+    destination_address_prefix = "10.0.1.0/24"
+  }
+    tags = {
+    environment = "Production"
+  }
+}
+
+  resource "azurerm__network_security_group" "ehealth_security_group_logic" {
+  name                = var.network_security_group_logic
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  security_rule {
+    name                       = "http"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "80"
+    source_address_prefix      = "10.0.1.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+    security_rule {
+    name                       = "https"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "443"
+    destination_port_range     = "443"
+    source_address_prefix      = "10.0.1.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+      security_rule {
+    name                       = "mysql"
+    priority                   = 104
+    direction                  = "outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "3306"
+    destination_port_range     = "3306"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.3.0/24"
+  }
+  tags = {
+    environment = "Production"
+  }
+}
+
+  resource "azurerm__network_security_group" "ehealth_security_group_data" {
+  name                = var.network_security_group_data
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  security_rule {
+    name                       = "mysql"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.3.0/24"
+  }
+  
+    tags = {
+    environment = "Production"
+  }
+}
+
+
+
+
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = module.vnet_module.subnet_prefixes[0]
+  network_security_group_id = azurerm_network_security_group.ehealth_security_group_web
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "logic" {
+  subnet_id                 = module.vnet_module.subnet_prefixes[0]
+  network_security_group_id = azurerm_network_security_group.ehealth_security_group_logic
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "data" {
+  subnet_id                 = module.module_vnet.subnet_prefixes[1 ]
+  network_security_group_id = azurerm_network_security_group.ehealth_security_group_data
+}
+
+
+resource "azurerm_lb" "ehealthLB" {
+  name                = "EhealthtLoadBalancer"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = module.module_vnet.public_ip_id
+  }
 }
