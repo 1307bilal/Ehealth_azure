9cc303ae434f23a03a4b5958158b759b4d375c40
ProcessX <luc2526@gmail.com>
2024-03-29 10:28:17 +0100

    Control Module added in Main

--- module_controlMachine/outputs.tf
+++ module_controlMachine/outputs.tf
@@ -1,12 +1,12 @@
-output "vm_id" {
-  value = azurerm_linux_virtual_machine.linuxvm.id
+output "control_vm_id" {
+  value = azurerm_linux_virtual_machine.control-vm.id
 }
 
-output "vm_ip" {
-  value = azurerm_linux_virtual_machine.linuxvm.public_ip_address
+output "control_vm_ip" {
+  value = azurerm_linux_virtual_machine.control-vm.public_ip_address
 }
 
-output "tls_private_key" {
-  value = tls_private_key.example_ssh.private_key_pem
+output "control_machine_key_ssh" {
+  value = tls_private_key.control_machine_key_ssh.private_key_pem
   sensitive = true
 }
