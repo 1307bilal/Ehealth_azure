0afc008fd32a60eac290137edb84ca4bd1809edf
ProcessX <luc2526@gmail.com>
2024-03-29 10:25:57 +0100

    variables set

--- module_controlMachine/main.tf
+++ module_controlMachine/main.tf
@@ -18,14 +18,14 @@ resource "azurerm_virtual_network" "control-vnet" {
   }
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "control-subnet" {
   name = var.subnet_name
   resource_group_name = azurerm_resource_group.control-rg.name
-  virtual_network_name = azurerm_virtual_network.vnet-linux.name
+  virtual_network_name = azurerm_virtual_network.control-vnet.name
   address_prefixes = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "public_ip" {
+resource "azurerm_public_ip" "control-public-ip" {
   name = var.public_ip_name
   resource_group_name = azurerm_resource_group.control-rg.name
   location = azurerm_resource_group.control-rg.location
@@ -35,7 +35,7 @@ resource "azurerm_public_ip" "public_ip" {
   }
 }
 
-resource "azurerm_network_security_group" "nsg" {
+resource "azurerm_network_security_group" "control-nsg" {
   name = var.network_security_group_name
   location = azurerm_resource_group.control-rg.location
   resource_group_name = azurerm_resource_group.control-rg.name
@@ -57,16 +57,16 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-resource "azurerm_network_interface" "nic"{
+resource "azurerm_network_interface" "control-nic"{
   name = var.network_interface_name
   location = azurerm_resource_group.control-rg.location
   resource_group_name = azurerm_resource_group.control-rg.name
 
   ip_configuration {
-    name = "myNicConfiguration"
-    subnet_id = azurerm_subnet.subnet.id
+    name = "control-nic-config"
+    subnet_id = azurerm_subnet.control-subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.public_ip.id
+    public_ip_address_id = azurerm_public_ip.control-public-ip.id
   }
 
   tags = {
@@ -74,20 +74,20 @@ resource "azurerm_network_interface" "nic"{
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "association" {
-  network_interface_id = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+resource "azurerm_network_interface_security_group_association" "control-sg-association" {
+  network_interface_id = azurerm_network_interface.control-nic.id
+  network_security_group_id = azurerm_network_security_group.control-nsg.id
 }
 
-resource "random_id" "randomId" {
+resource "random_id" "control-randomId" {
   keepers = {
     resource_group = azurerm_resource_group.control-rg.name
   }
   byte_length = 8
 }
 
-resource "azurerm_storage_account" "storage" {
-  name = "diag${random_id.randomId.hex}"
+resource "azurerm_storage_account" "control-storage" {
+  name = "diag${random_id.control-randomId.hex}"
   resource_group_name = azurerm_resource_group.control-rg.name
   location = azurerm_resource_group.control-rg.location
   account_tier = "Standard"
@@ -98,16 +98,16 @@ resource "azurerm_storage_account" "storage" {
   }
 }
 
-resource "tls_private_key" "example_ssh" {
+resource "tls_private_key" "control_machine_key_ssh" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
 
-resource "azurerm_linux_virtual_machine" "linuxvm" {
-  name = var.linux_virtual_machine_name
+resource "azurerm_linux_virtual_machine" "control-vm" {
+  name = var.control_machine_name
   resource_group_name = azurerm_resource_group.control-rg.name
   location = azurerm_resource_group.control-rg.location
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  network_interface_ids = [azurerm_network_interface.control-nic.id]
   size = "Standard_DS1_v2"
 
   os_disk {
@@ -123,17 +123,17 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
     version = "latest"
   }
 
-  computer_name = "myvm"
+  computer_name = "control-vm"
   admin_username = "azureuser"
   disable_password_authentication = true
 
   admin_ssh_key {
     username = "azureuser"
-    public_key = tls_private_key.example_ssh.public_key_openssh
+    public_key = tls_private_key.control_machine_key_ssh.public_key_openssh
   }
 
   boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
+    storage_account_uri = azurerm_storage_account.control-storage.primary_blob_endpoint
   }
 
   tags = {
