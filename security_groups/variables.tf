c95a8fba23535caa918beb630e3766f175a340f4
jihad-bf <jihad.spiridon@gmail.com>
2024-04-02 23:07:48 +0200

    corrections on all security groups files + main and variables from  main

--- security_groups/variables.tf
+++ security_groups/variables.tf
@@ -1,45 +1,50 @@
-variable "resource_group_name" {
+variable "resource_group_name"{
   description = "Name of the resource group to be imported."
   type        = string
 }
 
-variable "resource_group_location" {
+variable "resource_group_location"{
   description = "Location of ehealth resource group"
   type        = string
 }
 
-variable "network_security_group_web" {
+variable "network_security_group_web"{
   description = "The security group rules for web layer"
   type        = string
 }
 
 variable "network_security_group_logic" {
-  description = "The security grouo rules for logic layer"
+  description = "The security group rules for logic layer"
   type        = string
 }
 
-variable "network_security_group_data" {
+variable "network_security_group_data"{
   description = "The security group rules for data layer"
-  type = string
+  type        = string
 }
 
-variable "NSG group association Web"{
+variable "NSG_group_association_web"{
   description = "The group association for the web layer"
-  type = string
+  type        = string
 
 }
-variable "NSG group association Logic"{
+variable "NSG_group_association_logic"{
   description = "The group association for the logic layer"
-  type = string
+  type        = string
 }
 
-variable "NSG group association Data"{
+variable "NSG_group_association_data"{
   description = "The group association for the data layer"
-  type = string
+  type        = string
 }
 
-/**variable "network_security_group_in_nlb_web" {
+/**variable "network_security_group_in_nlb_web"{
   description = "The security group rules for nlb"
-  type = string
-}:
+  type        = string
+}
+
+variable "NSG_group_association_NLB"{
+  description = "The group association for the load balancer"
+  type        = string
+}
 **/
